837a48eeb0a9d7d6601c573ccf23af87f6b1c46f
adelinesharla <adelinesharla@duck.com>
2024-09-28 20:09:49 -0300

    fix: lint

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
   role_arn = var.aws_iam_role
   vpc_config {
-    subnet_ids         = [for subnet in aws_subnet.public : subnet.id]
+    subnet_ids         = var.subnet_ids
     endpoint_private_access = true
     endpoint_public_access = true
   }
