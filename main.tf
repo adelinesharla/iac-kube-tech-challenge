283b67f9266271db38fcc4444411887932fe87d1
adelinesharla <adelinesharla@duck.com>
2024-09-28 20:39:52 -0300

    fix: remove internet gateway creation

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = var.aws_region 
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = var.vpc_id
-}
-
 resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
   role_arn = var.aws_iam_role
