75c9a0e3fb183b8321dd70f8bd0601b4ee7b67d0
adelinesharla <adelinesharla@duck.com>
2024-09-28 20:10:57 -0300

    fix: subnet var name

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
   role_arn = var.aws_iam_role
   vpc_config {
-    subnet_ids         = var.subnet_ids
+    subnet_ids         = var.aws_subnets
     endpoint_private_access = true
     endpoint_public_access = true
   }
