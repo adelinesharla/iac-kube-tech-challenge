f766daabae1f3d9d7ea990c221553d37e2aefbbf
adelinesharla <adelinesharla@duck.com>
2024-09-29 17:26:38 -0300

    fix: sintax

--- main.tf
+++ main.tf
@@ -49,14 +49,14 @@ resource "aws_eks_cluster" "cluster" {
 }
 
 resource "aws_eks_access_entry" "eks-access-entry" {
-  cluster_name      = aws_eks_cluster.eks-cluster.name
+  cluster_name      = aws_eks_cluster.cluster.name
   principal_arn     = var.principalArn
   kubernetes_groups = ["my-nodes"]
   type              = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "eks-access-policy" {
-  cluster_name  = aws_eks_cluster.eks-cluster.name
+  cluster_name  = aws_eks_cluster.cluster.name
   policy_arn    = var.policyArn
   principal_arn = var.principalArn
 
