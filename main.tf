8da2ef77764f02b69f99db122cbcab41ed165aae
adelinesharla <adelinesharla@duck.com>
2024-09-29 19:43:19 -0300

    fix: sintax

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_eks_access_policy_association" "eks-access-policy" {
 }
 
 resource "aws_eks_node_group" "eks-node" {
-  cluster_name    = aws_eks_cluster.eks-cluster.name
+  cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = var.nodeGroup
   node_role_arn   = var.aws_iam_role
   subnet_ids      = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
