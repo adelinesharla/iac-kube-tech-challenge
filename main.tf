4770042e225680c46642996c09bebba25f5f5d6a
adelinesharla <adelinesharla@duck.com>
2024-09-29 15:58:25 -0300

    fix: adjust sg for nodes

--- main.tf
+++ main.tf
@@ -21,20 +21,20 @@ resource "aws_eks_cluster" "cluster" {
 resource "aws_security_group" "eks_nodes_sg" {
   name        = "${var.cluster_name}-nodes-sg"
   description = "Grupo de segurança para os nodes do cluster EKS"
-  vpc_id      = var.vpc_id  # Certifique-se de que 'var.vpc_id' esteja definida
+  vpc_id      = var.vpc_id
 
   # Regras de entrada
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    self        = true  # Permite comunicação entre os nodes do mesmo grupo de segurança
+    self        = true
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite tráfego HTTPS de entrada de qualquer lugar (ajuste conforme necessário)
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Regras de saída
@@ -42,7 +42,7 @@ resource "aws_security_group" "eks_nodes_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite tráfego de saída para qualquer lugar (ajuste conforme necessário)
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -61,7 +61,36 @@ resource "aws_eks_node_group" "my_node_group" {
 
   instance_types = ["t3.nano"]
 
-  vpc_security_group_ids = [aws_security_group.eks_nodes_sg.id]
-  
   depends_on = [aws_eks_cluster.cluster]
+
+  launch_template {
+    id      = aws_launch_template.eks_nodes_lt.id 
+    version = aws_launch_template.eks_nodes_lt.latest_version
+  }
+}
+
+# Launch Template para os Nodes
+resource "aws_launch_template" "eks_nodes_lt" {
+  name_prefix   = "${var.cluster_name}-node-group-lt-"
+  image_id      = data.aws_ami.eks_ami.id # Certifique-se de ter essa AMI definida
+  instance_type = "t3.nano"
+
+  network_interfaces {
+    security_groups = [aws_security_group.eks_nodes_sg.id]
+  }
+}
+
+# Buscar a AMI mais recente para EKS
+data "aws_ami" "eks_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
