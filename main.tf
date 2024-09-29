cef3d446f479ecfdb11b35b51812b9bd9e89dd13
adelinesharla <adelinesharla@duck.com>
2024-09-29 19:46:06 -0300

    fix: sintax

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_eks_node_group" "eks-node" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = var.nodeGroup
   node_role_arn   = var.aws_iam_role
-  subnet_ids      = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
+  subnet_ids      = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.aws_region}e"]
   disk_size       = 30
   instance_types  = [var.instance_type]
 
