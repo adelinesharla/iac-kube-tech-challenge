1bc30c246f60aab26992d46408c81796b5c45365
adelinesharla <adelinesharla@duck.com>
2024-09-29 15:50:06 -0300

    feat: add sg creation to nodes

--- main.tf
+++ main.tf
@@ -17,6 +17,35 @@ resource "aws_eks_cluster" "cluster" {
   }
 }
 
+# Grupo de Segurança para os Nodes
+resource "aws_security_group" "eks_nodes_sg" {
+  name        = "${var.cluster_name}-nodes-sg"
+  description = "Grupo de segurança para os nodes do cluster EKS"
+  vpc_id      = var.vpc_id  # Certifique-se de que 'var.vpc_id' esteja definida
+
+  # Regras de entrada
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true  # Permite comunicação entre os nodes do mesmo grupo de segurança
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite tráfego HTTPS de entrada de qualquer lugar (ajuste conforme necessário)
+  }
+
+  # Regras de saída
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite tráfego de saída para qualquer lugar (ajuste conforme necessário)
+  }
+}
+
 # Definição do Node Group
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.cluster.name
@@ -32,5 +61,7 @@ resource "aws_eks_node_group" "my_node_group" {
 
   instance_types = ["t3.nano"]
 
+  vpc_security_group_ids = [aws_security_group.eks_nodes_sg.id]
+  
   depends_on = [aws_eks_cluster.cluster]
 }
